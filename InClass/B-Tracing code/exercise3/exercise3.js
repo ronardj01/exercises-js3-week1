008175de9b98b64a1f503017cd62705438810352
Ronar de Jesus <ronardj01@gmail.com>
2021-04-09 17:05:57 +0200

    Completed B-Tracing-code ejxercise3

--- InClass/B-Tracing code/exercise3/exercise3.js
+++ InClass/B-Tracing code/exercise3/exercise3.js
@@ -1,10 +1,13 @@
-const getRepos = function(repoName) {
-  return fetch('https://api.github.com/users/' + repoName + '/repos')  
+const getRepos = function (repoName) {
+  return fetch('https://api.github.com/users/' + repoName + '/repos')
     .then(data => data.json())
-    .then(function(response) {
-      return response.map(function(rep) {
-		console.log(rep.name);
-        return rep.name;
+    .then(function (response) {
+      return response.map(function (rep) {
+        console.log(rep.name);
+        var paragraph = document.createElement("p");
+        paragraph.innerText = rep.name;
+        myElement.appendChild(paragraph);
+        //return rep.name;
       });
     });
 };
@@ -16,9 +19,9 @@ console.log("Loading...");
 
 
 var myElement = document.querySelector("#main");
-var paragraph = document.createElement("p"); 
-paragraph.innerText = migracodeRepos;
-myElement.appendChild(paragraph);
+//var paragraph = document.createElement("p");
+//paragraph.innerText = migracodeRepos;
+//myElement.appendChild(paragraph);
 
 //Task1. Fix the code
 //Task2. Create a <p> for each repository
